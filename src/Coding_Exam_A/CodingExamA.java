596693cba5d8413eccbd523fd8b2e44aaeb215cb
League Student <league@iMac-26.attlocal.net>
2019-08-01 19:58:00 -0700

    started coding test

--- src/Coding_Exam_A/CodingExamA.java
+++ src/Coding_Exam_A/CodingExamA.java
@@ -21,6 +21,46 @@ public class CodingExamA {
 		 * 
 		 * See the Coding_Exam_A_Demo.jar for an example of what the finished product should look like.
 		 */
+		
+		int robotCount = Integer.parseInt(JOptionPane.showInputDialog("How many robots?"));
+		String color = JOptionPane.showInputDialog("What color? (Red, Green, Blue)");
+		int sideCount = Integer.parseInt(JOptionPane.showInputDialog("How many sides per shape?"));
+		
+		int rX = 10;
+		int rY = 100;
+		Thread t = new Thread();
+		for(int i = 0; i < robotCount; i++) {
+			//starting location
+			rX += 100;
+			if(i%10 == 0) {
+				rY += 100;
+			}
+			//creating robot/setting color
+			Robot r = new Robot(rX, rY);
+			r.setSpeed(6);
+			color.toLowerCase();
+			if(color.equals("red")) {
+				r.setPenColor(200, 0, 0);;
+			}
+			if(color.equals("green")) {
+				r.setPenColor(0, 200, 0);;
+			}
+			if(color.equals("blue")) {
+				r.setPenColor(0, 0, 200);;
+			}
+			t = new Thread(()->{
+				r.penDown();
+				for(int j = 0; j< sideCount; j++) {
+					r.move(500/sideCount);
+					r.turn(360/sideCount);
+				}
+			
+			});
+		}
+
+		for(int i = 0; i < robotCount; i++) {
+			t.start();
+		}
 
 	}
 }
